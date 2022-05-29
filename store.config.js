16235ab2c92aba9cbe6e32a95ae5163eafc59659
Abraao-Santos <abraao.santos@rocky.ag>
2022-05-28 23:17:27 -0300

    Update store.config.js

--- store.config.js
+++ store.config.js
@@ -36,7 +36,7 @@ module.exports = {
 
   // Lighthouse CI
   lighthouse: {
-    server: process.env.BASE_SITE_URL || 'http://localhost:3000',
+    // server: process.env.BASE_SITE_URL || 'http://localhost:3000',
     pages: {
       home: '/',
       pdp: '/apple-magic-mouse-99988212/p',
